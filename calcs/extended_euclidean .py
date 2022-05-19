7c7ca45d130f1d149badc7173f3f6c43f87520b9
Delta.v <tmm.michalski@gmail.com>
2022-05-19 21:16:55 +0200

    implement extended euclidean algorithm

--- calcs/extended_euclidean .py
+++ calcs/extended_euclidean .py
@@ -1,26 +1,79 @@
+
+
+def fmt(x: str) -> str:
+    '''
+    Dodaje 4-znakowy padding do wartosci.
+    '''
+    return str(x).rjust(7)
 # rozszerzony algorytm euklidesa
 # np. 1001(-1) mod 1656 = 761
 # gdzie podstawa to 1001 i mod to 1656
-def extended_euclidean(a, b):
-    if b == 0:
-        gcd, s, t = a, 1, 0
-        return (gcd, s, t)
-    else:
-        s2, t2, s1, t1 = 1, 0, 0, 1
-        while b > 0:
-            q = a // b
-            r, s, t = (a - b * q), (s2 - q * s1), (t2 - q * t1)
-            a, b, s2, t2, s1, t1 = b, r, s1, t1, s, t
-        gcd, s, t = a, s2, t2
-        return (gcd, s, t)
 
 
-# Take input
-print("Input two non-negative integers a and b such that a>=b")
-a, b = map(int, input().split())
-x, y = a, b  # Store temporarily
-result = extended_euclidean(a, b)
-# Print the result
-print(f"gcd({x},{y})={result[0]}")
-print(
-    f"Linear combination gcd(a,b)=sa+tb:\n {result[0]}={result[1]}*{x}+{result[2]}*{y}")
+def extended_euclidean(a: int, n: int):
+    '''
+    Rozszerzony Algorytm Euklidesa. Przyjmuje dwie nieujemne liczby całkowite a, n, gdzie a >= n.
+    '''
+    x: int = n
+    y: int = a
+    print(f'{fmt("i")}{fmt("u(i)")}{fmt("up(i)")}{fmt("v(i)")}{fmt("vp(i)")}{fmt("n(i)")}{fmt("a(i)")}{fmt("q(i)")}{fmt("r(i)")}\n')
+    i: int = 0
+    u: int = 0
+    up: int = 1
+    v: int = 1
+    vp: int = 0
+    while True:
+        row: str = ''
+        # iteration
+        row += fmt(str(i))
+
+        # u(i)
+        row += fmt(str(u))
+
+        # up(i)
+        row += fmt(str(up))
+
+        # v(i)
+        row += fmt(str(v))
+
+        # vp(i)
+        row += fmt(str(vp))
+
+        # n(i)
+        row += fmt(str(n))
+
+        # a(i)
+        row += fmt(str(a))
+
+        # q(i) - coefficient
+        q = n // a
+        row += fmt(str(q))
+
+        # r(i) - remainder
+        r = n % a
+        row += fmt(str(r))
+
+        i += 1
+
+        temp_u = u
+        u = up - q * temp_u
+        up = temp_u
+
+        temp_v = v
+        v = vp - q * temp_v
+        vp = temp_v
+
+        n = a
+        a = r
+
+        print(row)
+        if r == 0:
+            break
+
+    print(f'\na^(-1) = v3 mod n = {temp_v} mod {x} = {temp_v % x}')
+    print(f'Sprawdzenie: {y} * {temp_v % x} mod {x} = {y * (temp_v % x) % x}')
+
+    return temp_v
+
+
+extended_euclidean(11, 125)
